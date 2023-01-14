0dc2c2f7e0fe71d7a7538c6bfd472434856a3a4c
Leo Robinovitch <leorobinovitch@gmail.com>
2023-01-14 18:31:58 -0500

    More

--- data.js
+++ data.js
@@ -1,9 +1,32 @@
 const data = [
   {
+    "id": "Years",
+    "perYear": 1,
+    "imgFile": "years.png",
+    "prefix": "You've enjoyed",
+    "suffix": "years together",
+  },
+  {
     "id": "Shabbat",
     "perWeek": 1,
-    "imgName": "challah.png"
-  }
+    "imgFile": "shabbat.png",
+    "prefix": "You've celebrated Shabbat",
+    "suffix": "times together",
+  },
+  {
+    "id": "Channukah Candles",
+    "perYear": 44,
+    "imgFile": "channukah_candles.png",
+    "prefix": "You've lit",
+    "suffix": "Channukah candles together",
+  },
+  {
+    "id": "Movies Seen",
+    "perWeek": 1.3,
+    "imgFile": "movies.png",
+    "prefix": "You've seen",
+    "suffix": "movies together",
+  },
 ]
 
 export default data
